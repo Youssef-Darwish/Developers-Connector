091826e690af3e12f34b420d03cdc709a1333888
Youssef Darwish <ydarwish@LPALXY0590.ejadaalex.local>
2019-12-18 13:27:05 +0200

    Schemas & JWT token added

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	monogURI: 'mongodb://youssef:18oct11feb2011@ds239578.mlab.com:39578/dev-connector'
+	monogURI: 'mongodb://youssef:18oct11feb2011@ds239578.mlab.com:39578/dev-connector',
+	secretOrKey : 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const keys = require('../../config/keys');
+
 //Load user model
 const User = require('../../models/User');
 
@@ -12,11 +15,12 @@ router.get('/test', (req, res) => {
 	res.json({ msg: 'Users route works' });
 });
 
-//@route GET api/users/register
+//@route POST api/users/register
 //@desc  Register a new user
 //@access Public
 router.post('/register', (req, res) => {
 
+	
 	User.findOne({ email: req.body.email }).then(user => {
 		if (user) {
 		  //errors.email = 'Email already exists';
@@ -27,14 +31,12 @@ router.post('/register', (req, res) => {
 			r: 'pg', // Rating
 			d: 'mm' // Default
 		  });
-	
 		  const newUser = new User({
 			name: req.body.name,
 			email: req.body.email,
 			avatar,
 			password: req.body.password
 		  });
-	
 		  bcrypt.genSalt(10, (err, salt) => {
 			bcrypt.hash(newUser.password, salt, (err, hash) => {
 			  if (err) throw err;
@@ -47,6 +49,46 @@ router.post('/register', (req, res) => {
 		  });
 		}
 	  });
+});
+
+//@route Post api/users/login
+//@desc  Login user (returning token)
+//@access Public
+router.post('/login', (req, res) => {
+
+	const email = req.body.email;
+	const password = req.body.password;
+
+	//Find user by email
+	User.findOne({email})
+		.then(user =>{
+			//chek if user doesn't exist
+			if(!user){
+				return res.status(404).json({email : "User not found"});
+			}
+			//Check Password
+			bcrypt.compare(password,user.password)
+				.then(isMatch => { 
+					//password match with stored hashed password
+					if(isMatch){
+						// User matched 
+						const payload = { id :user.id,name : user.name,avatar:user.avatar} // create jwt payload
+						//sign the token
+						jwt.sign(payload,keys.secretOrKey,{expiresIn : 3600},
+							(err,token) => {
+								res.json({
+									success : true,
+									token : 'Bearer ' + token
+								})
+						});
+
+					}else{
+						return res.status(400).json({password: "Incorrect Password"});
+					}
+				})
+		}).catch(err => {
+			return res.status(400).json(err);
+		})
 });
 
 
